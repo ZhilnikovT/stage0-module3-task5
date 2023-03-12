73739b0cf4989c0f57964aa338c03f8e21a0512e
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-12 19:30:25 +0300

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    public static void main(String[] args) {
+        TimeConvertor t = new TimeConvertor();
+        t.convert((float) 60.116);
+    }
+    
     public void convert(float minutes) {
+        System.out.println((float)(minutes * 60));
     }
 }
